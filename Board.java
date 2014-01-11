2890e61ecae1cedc0306fac2f968fe4b3e7d1880
chriskim511 <christopherkim511@gmail.com>
2014-01-10 20:39:25 -0500

    Update Board.java
    First version of Board, with standard methods

--- Board.java
+++ Board.java
@@ -1,3 +1,34 @@
-public class Board{
-	private ChessPiece[][] chessboard = new ChessPiece[8][8];	
+public class Board {
+    
+    private Object[][] _board; 
+    public Board() {
+	_board = new Object[8][8];
+    }
+    private Object get( int r, int c) {
+	return _board[r-1][c-1];
+    }
+    private boolean isEmpty(int r, int c) {
+	return _board[r-1][c-1] == null;
+    }
+    private Object set(int r, int c, Object newVal) {
+	Object originalpiece = _board[r-1][c-1];
+	_board[r-1][c-1] = newVal;
+	return originalpiece;
+    }
+    public String toString() {
+	String foo = "";
+	for (Object i[] : _board) {
+	    for (Object piece : i) {
+		foo += piece + " ";
+	    }
+	    foo += "\n\n";
+	}
+	return foo;
+    }
+    public static void main( String[] args ) {
+	Board board = new Board();
+	System.out.println("Board:");
+	System.out.println(board);
+    }
 }
+
