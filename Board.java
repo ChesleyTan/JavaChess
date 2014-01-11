5ca7b96e5958208c19004861134a373cd1a368ff
chriskim511 <christopherkim511@gmail.com>
2014-01-10 21:36:57 -0500

    Update Board.java
    Incorporated ChessPieces into the Board. Now shows a standard Chess Board set up.

--- Board.java
+++ Board.java
@@ -1,24 +1,54 @@
+// dedicated to Chesley
+
+import pieces.*;
+
 public class Board {
     
-    private Object[][] _board; 
+    private ChessPiece[][] _board; 
     public Board() {
-	_board = new Object[8][8];
+	_board = new ChessPiece[8][8];
+	int r = 0;
+	String color = "B";
+	for (ChessPiece[] row : _board) {
+	    for (ChessPiece piece : row) {
+		if (r == 0 || r == 7) {
+		    _board[r][0] = new Rook(color);
+		    _board[r][1] = new Knight(color);
+		    _board[r][2] = new Bishop(color);
+		    _board[r][3] = new Queen(color);
+                    _board[r][4] = new King(color);
+                    _board[r][5] = new Bishop(color);
+		    _board[r][6] = new Knight(color);
+                    _board[r][7] = new Rook(color);
+		}
+		else if (r == 1 || r == 6) {
+		    for (int i = 0; i < 8; i++) {
+			_board[r][i] = new Pawn(color);
+		    }
+		}
+		else {
+		    color = "W";
+		}
+               
+	    }
+	    r += 1;
+	}
     }
-    private Object get( int r, int c) {
+    private ChessPiece get( int r, int c) {
 	return _board[r-1][c-1];
     }
     private boolean isEmpty(int r, int c) {
 	return _board[r-1][c-1] == null;
     }
-    private Object set(int r, int c, Object newVal) {
-	Object originalpiece = _board[r-1][c-1];
+    private ChessPiece set(int r, int c, ChessPiece newVal) {
+	ChessPiece originalpiece = _board[r-1][c-1];
 	_board[r-1][c-1] = newVal;
 	return originalpiece;
     }
     public String toString() {
 	String foo = "";
-	for (Object i[] : _board) {
-	    for (Object piece : i) {
+	for (ChessPiece i[] : _board) {
+	    for (ChessPiece piece : i) {
 		foo += piece + " ";
 	    }
 	    foo += "\n\n";
