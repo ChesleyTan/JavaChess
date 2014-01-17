f2c59d9d1e0964383634af03c02e7128cb67f78a
Chesley Tan <chesleytan97@gmail.com>
2014-01-17 18:35:51 -0500

    Remove diagnostic print statements

--- pieces/Bishop.java
+++ pieces/Bishop.java
@@ -6,9 +6,7 @@ public class Bishop extends ChessPiece{
 	}
 	public boolean validMovement(int myXCoor, int myYCoor, int targXCoor, int targYCoor, Board board){
 		if (Math.abs(super.getSlope(myXCoor,myYCoor,targXCoor,targYCoor)) == 1.0){
-			System.out.println("Q1");
 			if (myXCoor < targXCoor && myYCoor < targYCoor){
-			System.out.println("Q2");
 				for (int i = myXCoor + 1,u = myYCoor + 1;i<targXCoor && u<targYCoor;u++,i++){
 					if (!board.isEmpty(i,u))
 						return false;
@@ -16,7 +14,6 @@ public class Bishop extends ChessPiece{
 				return true;
 			}	
 			else if (myXCoor > targXCoor && myYCoor > targYCoor){
-			System.out.println("Q3");
 				for (int i = myXCoor - 1,u = myYCoor - 1;i>targXCoor && u>targYCoor;u--,i--){
 					if (!board.isEmpty(i,u))
 						return false;
@@ -24,7 +21,6 @@ public class Bishop extends ChessPiece{
 				return true;
 			}
 			else if (myXCoor < targXCoor && myYCoor > targYCoor){
-			System.out.println("Q4");
 				for (int i = myXCoor + 1,u = myYCoor - 1;i<targXCoor && u>targYCoor;u--,i++){
 					if (!board.isEmpty(i,u))
 						return false;
@@ -32,7 +28,6 @@ public class Bishop extends ChessPiece{
 				return true;
 			}	
 			else if (myXCoor > targXCoor && myYCoor < targYCoor){
-			System.out.println("Q5");
 				for (int i = myXCoor - 1,u = myYCoor + 1;i>targXCoor && u<targYCoor;u++,i--){
 					if (!board.isEmpty(i,u))
 						return false;
@@ -40,7 +35,6 @@ public class Bishop extends ChessPiece{
 				return true;
 			}
 		}
-			System.out.println("Q1");
 		return false;
 	}
 	public boolean validAttack(int myXCoor, int myYCoor, int targXCoor, int targYCoor, Board board){
