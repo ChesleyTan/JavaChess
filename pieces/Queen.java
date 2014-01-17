80562975d791680c452b2759aec3fe2593d892ea
Chesley Tan <chesleytan97@gmail.com>
2014-01-17 18:47:39 -0500

    Fix validMovement() defaulting to true

--- pieces/Queen.java
+++ pieces/Queen.java
@@ -12,12 +12,14 @@ public class Queen extends ChessPiece{
 					if (!board.isEmpty(i,myYCoor))
 						return false;
 				}
+				return true;
 			}
 			else if (myXCoor > targYCoor){
 				for (int i = myXCoor -1;i>targXCoor;i--){
 					if (!board.isEmpty(i,myYCoor))
 						return false;
 				}
+				return true;
 			}
 		}
 		else if(slope == 1.0){
@@ -26,24 +28,28 @@ public class Queen extends ChessPiece{
 					if (!board.isEmpty(i,u))
 						return false;
 				}	
+				return true;
 			}	
 			else if (myXCoor > targXCoor && myYCoor > targYCoor){
 				for (int i = myXCoor - 1,u = myYCoor - 1;i>targXCoor && u>targYCoor;u--,i--){
 					if (!board.isEmpty(i,u))
 						return false;
 				}	
+				return true;
 			}
 			else if (myXCoor < targXCoor && myYCoor > targYCoor){
 				for (int i = myXCoor + 1,u = myYCoor - 1;i<targXCoor && u>targYCoor;u--,i++){
 					if (!board.isEmpty(i,u))
 						return false;
 				}	
+				return true;
 			}	
 			else if (myXCoor > targXCoor && myYCoor < targYCoor){
 				for (int i = myXCoor - 1,u = myYCoor + 1;i>targXCoor && u<targYCoor;u++,i--){
 					if (!board.isEmpty(i,u))
 						return false;
 				}	
+				return true;
 			}
 		}
 		else if (slope == 10.0){
@@ -52,15 +58,17 @@ public class Queen extends ChessPiece{
 					if (!board.isEmpty(myXCoor,i))
 						return false;
 				}
+				return true;
 			}
 			else if (myYCoor < targYCoor){
 				for (int i = myYCoor + 1;i<targYCoor;i++){
 					if (!board.isEmpty(myXCoor,i))
 						return false;
 				}
+				return true;
 			}
 		}
-		return true;
+		return false;
 	}
 	public boolean validAttack(int myXCoor, int myYCoor, int targXCoor, int targYCoor, Board board){
 		return validMovement(myXCoor,myYCoor,targXCoor,targYCoor,board);
